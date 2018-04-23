a2362b76ba7f1cec69777a1b9660a5eea4e9239c
Pawel Bus <pawelbus.dev@outlook.com>
2018-04-23 21:42:29 +0200

    Add my solution to script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log('Rozwiązanie według wskazówek z kursu:')
+console.log('Rozwiązanie według wskazówek z kursu:');
 
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'],
     maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'],
@@ -14,3 +14,28 @@ if (allNames.indexOf(newName) === -1) {
 else {
   console.log('Imię ' + newName + ' znajduje się już w tablicy.');
 }
+
+console.log();
+console.log('-----------------------------------------------------');
+console.log();
+console.log('Moje rozwiązanie:');
+
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'],
+    maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'],
+    allNames = femaleNames.concat(maleNames);
+
+function addName(newName) {
+  if (allNames.indexOf(newName) === -1) {
+    allNames.push(newName);
+    console.log('Do tablicy dodano imię ' + newName);
+    console.log('Teraz tablica wygląda następująco: ' + allNames.join(' | '));
+  }
+  else {
+    console.log('Imię ' + newName + ' znajduje się już w tablicy.');
+  }
+}
+
+addName('Marian');
+addName('Asia');
+addName('Paweł');
+addName('Marian');
